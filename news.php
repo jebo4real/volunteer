64bc3255db6203696dfce39be8172b9d33a60d1c
manafsulleyman <55247652+manafsulleyman@users.noreply.github.com>
2020-03-30 19:56:18 +0000

    Add files via upload

---
+++ news.php
@@ -0,0 +1,182 @@
+<?php
+include "layout/header.php";
+include "layout/menu.php";
+?>
+
+		<!-- Latest News -->
+
+	<div class="news">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="section_title_container text-center">
+						<div class="section_title">latest news</div>
+						<div class="section_subtitle">Be part of a community of people experiencing Good together.</div>
+					</div>
+				</div>
+			</div>
+			<div class="row news_row">
+
+				<!-- News Post -->
+				<div class="col-xl-4 col-lg-6 news_post_col">
+					<div class="news_post">
+						<div class="news_image"><img src="images/news_1.jpg" alt=""></div>
+						<div class="news_post_content">
+							<div class="news_post_title">
+								<a href="blog_single.html">Volunteer Recruiter? Then get ready for Do IT 3.0!</a>
+							</div>
+							
+							<div class="news_post_text">
+							<p> We’re counting down the weeks until the springtime release of the brand new Do IT platform.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- News Post -->
+				<div class="col-xl-4 col-lg-6 news_post_col">
+					<div class="news_post">
+						<div class="news_image"><img src="images/news_2.jpg" alt=""></div>
+						<div class="news_post_content">
+							<div class="news_post_title">
+								<a href="blog_single.html">Moderator joins Pope Francis in call to prayer for peace</a>
+							</div>
+						
+							<div class="news_post_text">
+								<p>Integer luctus diam acto scerisque consectetur. Vimus euismod nec lacus sit amet. Aenean interdus mid vitae blandit  porta. Quisque dignissim commodo congue efficitur...</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- News Post -->
+				<div class="col-xl-4 col-lg-6 news_post_col">
+					<div class="news_post">
+						<div class="news_image"><img src="images/news_3.jpg" alt=""></div>
+						<div class="news_post_content">
+							<div class="news_post_title">
+								<a href="blog_single.html">Church plays a major role in tackling loneliness</a>
+							</div>
+
+							<div class="news_post_text">
+								<p>Integer luctus diam acto scerisque consectetur. Vimus euismod nec lacus sit amet. Aenean interdus mid vitae blandit  porta. Quisque dignissim commodo congue efficitur...</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- News Post -->
+				<div class="col-xl-4 col-lg-6 news_post_col">
+					<div class="news_post">
+						<div class="news_image"><img src="images/news_4.jpg" alt=""></div>
+						<div class="news_post_content">
+							<div class="news_post_title">
+								<a href="blog_single.html">Family History Enthusiasts to Gather in Utah for RootsTech</a>
+							</div>
+
+							<div class="news_post_text">
+								<p>Integer luctus diam acto scerisque consectetur. Vimus euismod nec lacus sit amet. Aenean interdus mid vitae blandit  porta. Quisque dignissim commodo congue efficitur...</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- News Post -->
+				<div class="col-xl-4 col-lg-6 news_post_col">
+					<div class="news_post">
+						<div class="news_image"><img src="images/news_5.jpg" alt=""></div>
+						<div class="news_post_content">
+							<div class="news_post_title">
+								<a href="blog_single.html">Faith and Rebuilding Will Make Puerto Ricans Stronger</a>
+							</div>
+							
+							<div class="news_post_text">
+								<p>Integer luctus diam acto scerisque consectetur. Vimus euismod nec lacus sit amet. Aenean interdus mid vitae blandit  porta. Quisque dignissim commodo congue efficitur...</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- News Post -->
+				<div class="col-xl-4 col-lg-6 news_post_col">
+					<div class="news_post">
+						<div class="news_image"><img src="images/news_6.jpg" alt=""></div>
+						<div class="news_post_content">
+							<div class="news_post_title">
+								<a href="blog_single.html">Historian Shares Legacy of Black Mormon Pioneers</a>
+							</div>
+							
+							<div class="news_post_text">
+								<p>Integer luctus diam acto scerisque consectetur. Vimus euismod nec lacus sit amet. Aenean interdus mid vitae blandit  porta. Quisque dignissim commodo congue efficitur...</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- News Post -->
+				<div class="col-xl-4 col-lg-6 news_post_col">
+					<div class="news_post">
+						<div class="news_image"><img src="images/news_7.jpg" alt=""></div>
+						<div class="news_post_content">
+							<div class="news_post_title">
+								<a href="blog_single.html">2018 Schedule for Church Pageants Now Available on LDS</a>
+							</div>
+							
+							<div class="news_post_text">
+								<p>Integer luctus diam acto scerisque consectetur. Vimus euismod nec lacus sit amet. Aenean interdus mid vitae blandit  porta. Quisque dignissim commodo congue efficitur...</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- News Post -->
+				<div class="col-xl-4 col-lg-6 news_post_col">
+					<div class="news_post">
+						<div class="news_image"><img src="images/news_8.jpg" alt=""></div>
+						<div class="news_post_content">
+							<div class="news_post_title">
+								<a href="blog_single.html">Former Church Presidents, Apostles Reflect on Calls</a>
+							</div>
+							
+							<div class="news_post_text">
+								<p>Integer luctus diam acto scerisque consectetur. Vimus euismod nec lacus sit amet. Aenean interdus mid vitae blandit  porta. Quisque dignissim commodo congue efficitur...</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- News Post -->
+				<div class="col-xl-4 col-lg-6 news_post_col">
+					<div class="news_post">
+						<div class="news_image"><img src="images/news_9.jpg" alt=""></div>
+						<div class="news_post_content">
+							<div class="news_post_title">
+								<a href="blog_single.html">All Saints: The Surprising True Story of How Refugees from Burma</a>
+							</div>
+							
+							<div class="news_post_text">
+								<p>Integer luctus diam acto scerisque consectetur. Vimus euismod nec lacus sit amet. Aenean interdus mid vitae blandit  porta. Quisque dignissim commodo congue efficitur...</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+			<!-- Page Nav -->
+
+			<div class="row">
+				<div class="col">
+					<div class="page_nav">
+						<ul>
+							<li class="active"><a href="#">1</a></li>
+							<li><a href="#">2</a></li>
+							<li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+<?php include "layout/footer.php"; ?>	
